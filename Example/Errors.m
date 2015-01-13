7042c5efbefdfcafd2cb844964bd51d3ca9c7bd2
Tuomas Kareinen <tkareine@gmail.com>
2015-01-13 23:13:13 +0200

    Set error only if pointer exists

--- Example/Errors.m
+++ Example/Errors.m
@@ -2,8 +2,10 @@
 
 void tfe_error(NSInteger errorCode, NSString *description, NSError **err)
 {
-  NSDictionary *info = @{NSLocalizedDescriptionKey: description};
-  *err = [NSError errorWithDomain:@"org.tkareine.ToyFuture.example"
-                             code:errorCode
-                         userInfo:info];
+  if (err) {
+    NSDictionary *info = @{NSLocalizedDescriptionKey: description};
+    *err = [NSError errorWithDomain:@"org.tkareine.ToyFuture.example"
+                               code:errorCode
+                           userInfo:info];
+  }
 }
